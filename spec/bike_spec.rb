545bf0bc34607bf6547a59b55d8d653d7290c713
iainaitken <iain_aitken@yahoo.co.uk>
2021-02-04 17:26:08 +0000

    Revert "update files after challenge 18 and prior to revert"
    This reverts commit f74a8999ded7fea4203fab14bfe9f2a258f3ed80.

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,14 +1,8 @@
 require 'docking_station'
 
 describe 'Bike' do
-
   it 'responds to release_bike' do
     bike = Bike.new
-    expect(bike.working?).to be(true)
-  end
-
-  it 'allows user to report broken bike' do
-    bike = Bike.new
-    expect { bike.report_broken }.to change { bike.broken }.from(false).to(true)
+    expect(bike).to respond_to(:working?)
   end
 end
