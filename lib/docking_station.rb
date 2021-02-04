7a7286fb828271f2501520aff087525d94246fee
iainaitken <iain_aitken@yahoo.co.uk>
2021-02-04 14:17:39 +0000

    update files following challenges 13 and 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,19 +3,23 @@ class DockingStation
   attr_reader :bikes
 
   def initialize
-  @bikes = 0
+    @bikes = []
   end
 
   def release_bike
-    if @bikes > 0
+    if @bikes.length > 0
       Bike.new
     else
       raise Exception.new "no bikes here"
     end
   end
 
-  def dock_bike
-    @bikes += 1
+  def dock_bike(bike)
+    if @bikes.length < 20
+      @bikes << bike
+    elsif @bikes.length >= 20
+      raise("bike rack is full")
+    end
   end
 end
 
