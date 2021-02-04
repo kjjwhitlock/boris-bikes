f9b92eb0a7f596ab5dece8a784f7c76317bd893e
iainaitken <iain_aitken@yahoo.co.uk>
2021-02-04 16:01:48 +0000

    update files after challenge 17

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,12 @@
 # in lib/docking_station.rb
 class DockingStation
-  attr_reader :bikes
+  attr_reader :bikes, :capacity
 
   DEFAULT_CAPACITY = 20
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @bikes = []
+    @capacity = capacity
   end
 
   def release_bike
@@ -26,7 +27,7 @@ class DockingStation
 
   private
   def full?
-    if @bikes.length == DEFAULT_CAPACITY
+    if @bikes.length == @capacity
       true
     else
       false
