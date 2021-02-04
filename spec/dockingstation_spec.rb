33cd85c0a35a55e0d36c97259e45634514c3f6b1
iainaitken <iain_aitken@yahoo.co.uk>
2021-02-04 17:25:50 +0000

    Revert "Revert "Revert "update files after challenge 17"""
    This reverts commit 0241555b7012a2bcd7fe02a57c00fbd934b0f330.

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,31 +1,25 @@
 require 'docking_station'
 
 describe 'DockingStationTest' do
-
   it 'responds to release_bike' do
     expect(DockingStation.new).to respond_to(:release_bike)
   end
-
   it 'get the bike and check if bike is working' do
     station = DockingStation.new
     station.dock(Bike.new)
     expect(station.release_bike).to respond_to(:working?)
   end
-
   it 'docks the bike' do
     expect(DockingStation.new).to respond_to(:dock)
   end
-
   it 'docking the bike increases total of bikes in docking station' do
     station = DockingStation.new
     expect { station.dock(Bike.new) }.to change{station.bikes.length}.from(0).to(1)
   end
-
   it 'raises an error if user tries to release non-existent bike' do
     station = DockingStation.new
     expect { station.release_bike }.to raise_exception("no bikes here")
   end
-
   it 'raises an error if user tries to dock bike when station full' do
     station = DockingStation.new
     bike = Bike.new
@@ -33,6 +27,7 @@ describe 'DockingStationTest' do
     expect { test.times { station.dock(bike) } }.to raise_error("bike rack is full")
   end
 
+<<<<<<< HEAD
   it 'allows user to set default bike capacity' do
     station = DockingStation.new(30)
     expect { station }.to_not raise_error
@@ -59,4 +54,6 @@ describe 'DockingStationTest' do
     expect { station.dock(bike) }.to change { station.bikes }.from([]).to([bike])
   end
 
+=======
+>>>>>>> parent of f9b92eb (update files after challenge 17)
 end
